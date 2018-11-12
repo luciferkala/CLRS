4f43a4da883e89d7eb9a2ca65f9a7663b595b857
maestroprog <maestroprog@naver.com>
2018-11-12 19:15:36 +0900

    create Xcode Project

---
+++ CLRS_Chap2/CLRS_Chap2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CLRS_Chap2
+//
+//  Created by Yang Soohan on 2018. 11. 12..
+//  Copyright © 2018년 Yang Soohan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
